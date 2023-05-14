9c14866763c8f1d06a0555bbacda22b407504757
Joost de Niet <17049644@student.hhs.nl>
2023-05-14 14:10:21 +0200

    Added sound and model for tactical insertion feature

--- events/player_connected_watch_events.lua
+++ events/player_connected_watch_events.lua
@@ -5,20 +5,56 @@ function PlayerConnectedWatchEvents(player)
     player:notifyonplayercommand("cancel_claymore", "+actionslot 3")
     player:onnotify("cancel_claymore", function()
         if player.type == "zombie" then
-
             local weapons = player:getweaponslistall()
             for i = 1, #weapons do
                 if weapons[i] == "h1_claymore_mp" then
-                    player:takeallweapons()
-                    player.savedPosistion = player.origin
-                    player.savedAngle = player.angles
-                    player:iprintlnbold("^2Tactical insertion is set")
-
-                    game:ontimeout(function()
-                        player:GiveZombieClassClaymore()
-                    end, 300)
+                    player:SpawnClaymore()            
                 end
             end
         end
     end)
+end
+
+-- [[ Spawn Tactical Insertion for Player ]] --
+function entity:SpawnClaymore()
+    -- Save posistion
+    self.savedPosistion = self.origin
+    self.savedAngle = self.angles
+
+    -- Play effects
+    self:playlocalsound("claymore_plant_concrete")
+    game:ontimeout(function()
+        self:playlocalsound("claymore_plant_concrete")
+        game:ontimeout(function()
+            self:playlocalsound("claymore_plant_concrete")
+        end, 50)
+    end, 50)
+
+    -- Add delete Tactical Insersion function
+    local tactical = game:spawn("script_model",  self.origin)
+    tactical:setmodel("h1_dogtag_friend_animated")
+    tactical:show()
+    tactical:makeusable()
+    tactical:sethintstring("Press ^3[{+activate}]^7 to ^1destroy ^7the tactical insertion")
+
+    --local tacticalEffect = game:loadfx("vfx/unique/vfx_marker_dom")
+    --local playtacticalEffect = game:playfx(tacticalEffect, self.origin)
+
+    -- Wait for players trigger
+    tactical:onnotify("trigger", function(self)
+        if self.type == "zombie" then
+            self:iprintlnbold("You cannot delete a tactical insertion!");
+            return
+        end
+
+        tactical:delete()
+        self:playlocalsound("bullet_large_ice")
+    end)
+
+    -- Other
+    self:iprintlnbold("^2Tactical insertion is set")
+
+    game:ontimeout(function()
+        self:GiveZombieClassClaymore()
+    end, 300)
 end
