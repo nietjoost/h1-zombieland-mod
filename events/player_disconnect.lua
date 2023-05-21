672a2eb88dae826d8d2abe5e2ac511a1d3bc0e8e
Joost de Niet <17049644@student.hhs.nl>
2023-05-21 15:00:25 +0200

    Add player left the server console message

--- events/player_disconnect.lua
+++ events/player_disconnect.lua
@@ -5,6 +5,11 @@ function PlyerDisconnected(player)
         player:RemovePlayerFromTable(survivors)
         player:RemovePlayerFromTable(zombies)
 
+        -- LOG to server console
+        if player:is_bot() == false then
+            print("Player " .. player.name .. " LEFT the server!")
+        end
+
         CheckForPlayers()
     end)
 end
